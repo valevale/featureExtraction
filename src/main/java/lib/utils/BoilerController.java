73f556391078b917f9f36aeb94ec3869890bfd5a
valevale <valentinailluminati1992@gmail.com>
2016-09-15 17:31:07 +0200

    code refactoring

--- src/main/java/lib/utils/BoilerController.java
+++ src/main/java/lib/utils/BoilerController.java
@@ -23,127 +23,116 @@ public class BoilerController {
 
 	public BoilerController() {
 	}
-	
+
 	public List<PageEntry> getUsedPagesForCleaning() {
 		return this.usedPagesForCleaning;
 	}
 
 	/*prende una pagina e individua il template da eliminare*/
+	@SuppressWarnings("rawtypes")
 	public String boilPages(PageEntry pageToClean) throws Exception {
+		//passo 1. prendo N pagine da pulire
+		String source = pageToClean.getCrawlingId();
+		Iterator<PageEntry> iterator = facade.pageEntryIterator(source);
+		int counter=0;
+		
+		//ora iteriamo fino a un certo numero le pagine, facendo attenzione che non ci abbiano restituito la
+		//pageEntry che già abbiamo!
+		try {
+			while (iterator.hasNext() && counter<numberCleanedPages) {
+				System.out.println("NUMERO DI PAGINE PRESE "+counter);
+				PageEntry page = iterator.next();
+				if (page.getId() != pageToClean.getId()) {
+					this.usedPagesForCleaning.add(page);
+					counter++;
+				}
+			}
+		} finally {
+			((MorphiaIterator) iterator).close();
+		}
 
 		//TODO refactoring del codice: parametrizza questi make e get, non è difficile
-//		HashSet<BlacklistElement> blacklistedTxt = getBlackListTxt(pageToClean);
-//		HashSet<BlacklistElement> blacklistedImg = getBlackListImg(pageToClean);
-		HashSet<BlacklistElement> blacklistedTxt = makeBlackListTxt(pageToClean);
-		HashSet<BlacklistElement> blacklistedImg = makeBlackListImg(pageToClean);
+		//		HashSet<BlacklistElement> blacklistedTxt = getBlackListTxt(pageToClean);
+		//		HashSet<BlacklistElement> blacklistedImg = getBlackListImg(pageToClean);
+		HashSet<BlacklistElement> blacklistedTxt = makeBlackList(pageToClean, "txt");
+		HashSet<BlacklistElement> blacklistedImg = makeBlackList(pageToClean, "img");
 
 		//ora, con le due liste, posso filtrare la mia pagina
 		Document documentToClean = Jsoup.parse(pageToClean.getPage().getBody());
 		CleanHTMLTree.travers(documentToClean, blacklistedTxt, blacklistedImg);
 		Document docToCleanParsed = Jsoup.parse(documentToClean.text());
-		
+
 		return docToCleanParsed.text();
 	}
-	
+
 	/*data una pagina, restituisce la blacklist*/
-//	public HashSet<BlacklistElement> getBlackListTxt(PageEntry page) throws Exception {
-//		Source site = facade.getSite(page);
-//		if (site.getBlacklistedTxt() == null) {
-//			return makeBlackListTxt(site);
-//		}
-//		return site.getBlacklistedTxt();
-//	}
-//	
-//	/*data una pagina, restituisce la blacklist*/
-//	public HashSet<BlacklistElement> getBlackListImg(PageEntry page) throws Exception {
-//		Source site = facade.getSite(page);
-//		if (site.getBlacklistedImg() == null) {
-//			return makeBlackListImg(site);
-//		}
-//		return site.getBlacklistedImg();
-//	}
+	//	public HashSet<BlacklistElement> getBlackListTxt(PageEntry page) throws Exception {
+	//		Source site = facade.getSite(page);
+	//		if (site.getBlacklistedTxt() == null) {
+	//			return makeBlackListTxt(site);
+	//		}
+	//		return site.getBlacklistedTxt();
+	//	}
+	//	
+	//	/*data una pagina, restituisce la blacklist*/
+	//	public HashSet<BlacklistElement> getBlackListImg(PageEntry page) throws Exception {
+	//		Source site = facade.getSite(page);
+	//		if (site.getBlacklistedImg() == null) {
+	//			return makeBlackListImg(site);
+	//		}
+	//		return site.getBlacklistedImg();
+	//	}
 
 	/* dato un sito Source, crea e memorizza la blacklistTxt, salvandola anche nel database */
-	@SuppressWarnings("rawtypes")
-	public HashSet<BlacklistElement> makeBlackListTxt(PageEntry pageToClean) throws Exception {
-		//passo 1. prendo N pagine da pulire
-		String source = pageToClean.getCrawlingId();
-		Iterator<PageEntry> iterator = facade.pageEntryIterator(source);
-		
-		//ora iteriamo fino a un certo numero le pagine, facendo attenzione che non ci abbiano restituito la
-		//pageEntry che già abbiamo!
-		try {
-			int i=0;
-			while (iterator.hasNext() && i<=numberCleanedPages && iterator.next() != null) {
-				PageEntry page = iterator.next();
-				if (page.getId() != pageToClean.getId()) {
-					this.usedPagesForCleaning.add(page);
-					i++;
-				}
-			}
-		} finally {
-			((MorphiaIterator) iterator).close();
-		}
-		
-		HashSet<BlacklistElement> blacklistedTxt = new HashSet<>();
+	public HashSet<BlacklistElement> makeBlackList(PageEntry pageToClean, String parameter) throws Exception {
+
+		HashSet<BlacklistElement> blacklist = new HashSet<>();
 		if (this.usedPagesForCleaning.size() > 1) {
 			PageEntry page = this.usedPagesForCleaning.get(0);
 			Document document = Jsoup.parse(page.getPage().getBody());
-			blacklistedTxt = CleanHTMLTree.getHTMLElementsText(document);
+			if (parameter.equals("txt"))
+				blacklist = CleanHTMLTree.getHTMLElementsText(document);
+			else //img
+				blacklist = CleanHTMLTree.getHTMLElementsImg(document);
 
 			for (int i=1; i<this.usedPagesForCleaning.size();i++) {
 				page = this.usedPagesForCleaning.get(i);
 				document = Jsoup.parse(page.getPage().getBody());
-				HashSet<BlacklistElement> tempTxt = CleanHTMLTree.getHTMLElementsText(document);
-				blacklistedTxt.retainAll(tempTxt);
-				tempTxt.clear();
+				HashSet<BlacklistElement> temp;
+				if (parameter.equals("txt"))
+					temp = CleanHTMLTree.getHTMLElementsText(document);
+				else //img
+					temp = CleanHTMLTree.getHTMLElementsImg(document);
+				blacklist.retainAll(temp);
+				temp.clear();
 			}
 			//TODO poi fallo!!!
 			//facade.updateBlackList(site, blacklistedTxt);	
-			return blacklistedTxt;
+			return blacklist;
 		}
-		return blacklistedTxt;
+		return blacklist;
 	}
-	
-	/* dato un sito Source, crea e memorizza la blacklistTxt, salvandola anche nel database */
-	@SuppressWarnings("rawtypes")
-	public HashSet<BlacklistElement> makeBlackListImg(PageEntry pageToClean) throws Exception {
-		//passo 1. prendo N pagine da pulire
-		String source = pageToClean.getCrawlingId();
-		Iterator<PageEntry> iterator = facade.pageEntryIterator(source);
-		
-		//ora iteriamo fino a un certo numero le pagine, facendo attenzione che non ci abbiano restituito la
-		//pageEntry che già abbiamo!
-		try {
-			int i=0;
-			while (iterator.hasNext() && i<=numberCleanedPages) {
-				PageEntry page = iterator.next();
-				if (page.getId() != pageToClean.getId()) {
-					this.usedPagesForCleaning.add(page);
-					i++;
-				}
-			}
-		} finally {
-			((MorphiaIterator) iterator).close();
-		}
-		
-		HashSet<BlacklistElement> blacklistedImg = new HashSet<>();
-		if (this.usedPagesForCleaning.size() > 1) {
-			PageEntry page = this.usedPagesForCleaning.get(0);
-			Document document = Jsoup.parse(page.getPage().getBody());
-			blacklistedImg = CleanHTMLTree.getHTMLElementsImg(document);
 
-			for (int i=1; i<this.usedPagesForCleaning.size();i++) {
-				page = this.usedPagesForCleaning.get(i);
-				document = Jsoup.parse(page.getPage().getBody());
-				HashSet<BlacklistElement> tempImg = CleanHTMLTree.getHTMLElementsImg(document);
-				blacklistedImg.retainAll(tempImg);
-				tempImg.clear();
-			}
-			//TODO poi fallo!!!
-			//IMMAGINE NON TXT facade.updateBlackList(site, blacklistedTxt);	
-			return blacklistedImg;
-		}
-		return blacklistedImg;
-	}
+	/* dato un sito Source, crea e memorizza la blacklistTxt, salvandola anche nel database */
+	//	@SuppressWarnings("rawtypes")
+	//	public HashSet<BlacklistElement> makeBlackListImg(PageEntry pageToClean) throws Exception {
+	//		HashSet<BlacklistElement> blacklistedImg = new HashSet<>();
+	//		if (this.usedPagesForCleaning.size() > 1) {
+	//			PageEntry page = this.usedPagesForCleaning.get(0);
+	//			Document document = Jsoup.parse(page.getPage().getBody());
+	//			blacklistedImg = CleanHTMLTree.getHTMLElementsImg(document);
+	//
+	//			for (int i=1; i<this.usedPagesForCleaning.size();i++) {
+	//				page = this.usedPagesForCleaning.get(i);
+	//				document = Jsoup.parse(page.getPage().getBody());
+	//				HashSet<BlacklistElement> tempImg = CleanHTMLTree.getHTMLElementsImg(document);
+	//				blacklistedImg.retainAll(tempImg);
+	//				tempImg.clear();
+	//			}
+	//			//TODO poi fallo!!!
+	//			//IMMAGINE NON TXT facade.updateBlackList(site, blacklistedTxt);	
+	//			return blacklistedImg;
+	//		}
+	//		return blacklistedImg;
+	//	}
 }
