de8d65a2d63c8849a9a433c499eef2c6d2dbd7cb
valevale <valentinailluminati1992@gmail.com>
2016-11-11 17:48:56 +0100

    segmentation task updated

--- src/main/java/otherSegmentation/LuceneConfiguration.java
+++
@@ -1,6 +0,0 @@
-package otherSegmentation;
-
-public class LuceneConfiguration {
-    public static final String SOURCE_DIRECTORY_TO_INDEX = "E:/TEST";
-    public static final String INDEX_DIRECTORY = "E:/INDEXDIRECTORY";
-}
