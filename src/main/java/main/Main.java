5957c64ac7fd89561ff204732ed3caa33e859502
valevale <valentinailluminati1992@gmail.com>
2017-02-19 09:34:16 +0100

    test di tutto il processo

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -43,48 +43,50 @@ public class Main {
 		for (int i=0;i<SourceInput.getSorgenti().size();i++) {
 			String domain = SourceInput.getSorgenti().get(i);
 			Profile currentProfile = domains2profile.get(domain);
-			String id = currentProfile.getIdDomain();
-			//per ogni profilo  prendi il dominio
-			MongoFacade facade = new MongoFacade("web_search_pages");
-			System.out.println("*****PRENDO UNA NUOVA SORGENTE");
-			Source currentSource = facade.getSourceWithId(id);
-			String json = "[";
-			//per ogni dominio prendi le pagine web
-			//TODO cambia
-			//			for (int j=0;j<currentSource.getPages().size();j++) {
-			for (int j=0;j<100;j++) {
-				System.out.println("*****pagina numero: "+(j+1)+"/"+currentSource.getPages().size());
-				//per ogni pagina applichi le xpath del profilo
-				WebPage currentPage = currentSource.getPages().get(j);
-				List<String> contents = currentProfile.getContentInformation(currentPage, currentSource.getId().toString());
-				//ora faccio un json
-				json = json + "{"+
-						"url: \""+currentPage.getUrl()+"\", \n";
-				for (int c=0; c<contents.size();c++) {
-					String currentContent=contents.get(c);
-					currentContent = currentContent.replaceAll("\"", "");
-					String pathCode=currentProfile.getMatchingInformation().get(c);
-					//					json = json + "xp"+pathCode+": \""+currentContent+"\", \n";
-					json = json + "xp"+pathCode+": \""+currentContent+"\", \n";
+			if (currentProfile != null) {
+				String id = currentProfile.getIdDomain();
+				//per ogni profilo  prendi il dominio
+				MongoFacade facade = new MongoFacade("web_search_pages");
+				System.out.println("*****PRENDO UNA NUOVA SORGENTE");
+				Source currentSource = facade.getSourceWithId(id);
+				String json = "[";
+				//per ogni dominio prendi le pagine web
+				//TODO cambia
+				//			for (int j=0;j<currentSource.getPages().size();j++) {
+				for (int j=0;j<100;j++) {
+					System.out.println("*****pagina numero: "+(j+1)+"/"+currentSource.getPages().size());
+					//per ogni pagina applichi le xpath del profilo
+					WebPage currentPage = currentSource.getPages().get(j);
+					List<String> contents = currentProfile.getContentInformation(currentPage, currentSource.getId().toString());
+					//ora faccio un json
+					json = json + "{"+
+							"url: \""+currentPage.getUrl()+"\", \n";
+					for (int c=0; c<contents.size();c++) {
+						String currentContent=contents.get(c);
+						currentContent = currentContent.replaceAll("\"", "");
+						String pathCode=currentProfile.getMatchingInformation().get(c);
+						//					json = json + "xp"+pathCode+": \""+currentContent+"\", \n";
+						json = json + "xp"+pathCode+": \""+currentContent+"\", \n";
+					}
+					json = json.substring(0, json.length()-2);
+					json = json + "}, \n";
+
 				}
 				json = json.substring(0, json.length()-2);
-				json = json + "}, \n";
+				json = json + "]";
+				//salvi in un json le info
+				//un json per dominio
+				PrintWriter testPrinter = new PrintWriter(path+"profiliDominio_"+
+						domain.substring(domain.length()-4, domain.length())
+				+".json", "UTF-8");
 
+				testPrinter.println(json);
+
+				testPrinter.close();
+				System.out.println("*****HO STAMPATO UN NUOVO JSON!");
+				//poi elimina la sorgente, per risparmiare spazio
+				currentSource = null;
 			}
-			json = json.substring(0, json.length()-2);
-			json = json + "]";
-			//salvi in un json le info
-			//un json per dominio
-			PrintWriter testPrinter = new PrintWriter(path+"profiliDominio_"+
-					domain.substring(domain.length()-4, domain.length())
-			+".json", "UTF-8");
-
-			testPrinter.println(json);
-
-			testPrinter.close();
-			System.out.println("*****HO STAMPATO UN NUOVO JSON!");
-			//poi elimina la sorgente, per risparmiare spazio
-			currentSource = null;
 		}
 	}
 
