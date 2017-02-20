0ddde1850dea2f760e7e4c9d4b347674f6b2d078
valevale <valentinailluminati1992@gmail.com>
2017-02-20 14:01:45 +0100

    procedura estesa

--- src/main/java/main/SourceInput.java
+++ src/main/java/main/SourceInput.java
@@ -10,7 +10,7 @@ public class SourceInput {
 	public static void inizializzaLista() {
 		idSorgenti.add("5750678b3387e31f516fa1c7");
 		idSorgenti.add("5750678b3387e31f516fa1d0");
-		idSorgenti.add("5750678b3387e31f516fa1ca");
+//		idSorgenti.add("5750678b3387e31f516fa1ca");
 		idSorgenti.add("575067b33387e31f516face0");
 		idSorgenti.add("5750678b3387e31f516fa1cd");
 		idSorgenti.add("5750678a3387e31f516fa185");
